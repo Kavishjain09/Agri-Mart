1813c0f565323dbbbea484f187e45beefa623e1a
Kavish Dhirawat <64060642+Kavishjain09@users.noreply.github.com>
2025-02-14 22:18:27 +0530

    Delete Front directory

--- Front/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'http://localhost:8081/FarmersMarketplace/',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
